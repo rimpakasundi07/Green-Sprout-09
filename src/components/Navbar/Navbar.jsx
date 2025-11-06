c683198d445e90ddbb8e594ce59d53e48218c469
rimpa kasundi <rimpakasundi@gmail.com>
2025-11-06 21:16:50 +0530

    Update Navbar: add user icon image and improve layout styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,12 @@
 import React from "react";
 import { NavLink } from "react-router";
+import userImg from "../../assets/icons8-user-50.png";
 
 const Navbar = () => {
   return (
     <div className="navbar py-5">
       <div className="navbar-start">
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              {" "}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />{" "}
-            </svg>
-          </div>
           <ul
             tabIndex="-1"
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
@@ -43,8 +27,9 @@ const Navbar = () => {
           <NavLink to="/profile">My Profile</NavLink>
         </ul>
       </div>
-      <div className="navbar-end">
-        <a className="btn">Login</a>
+      <div className="navbar-end gap-5 items-center ">
+        <img src={userImg} alt="" />
+        <button className="btn">Login</button>
       </div>
     </div>
   );

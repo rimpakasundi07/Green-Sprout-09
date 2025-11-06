d4dad7ad171a2d0d20fa4668faf8af40a53fd612
rimpa kasundi <rimpakasundi@gmail.com>
2025-11-06 17:55:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="lg:text-6xl text-2xl font-bold">
+        Let's Build the Green sprout Project
+      </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Routes/Router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h2 className="text-2xl">Home Layout</h2>,
+  },
+]);
+
+export default router;

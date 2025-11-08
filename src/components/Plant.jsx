0a0c60474c734d06f3f32fae1ea973e9864189f3
rimpa kasundi <rimpakasundi@gmail.com>
2025-11-08 18:08:05 +0530

    Add arrow icon and style details button in Plant component

--- src/components/Plant.jsx
+++ src/components/Plant.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router";
+import arrowImg from "../assets/icons8-right-arrow-24.png";
 
 const Plant = ({ plant }) => {
   console.log(plant);
@@ -7,7 +8,7 @@ const Plant = ({ plant }) => {
   return (
     <div>
       <Link>
-        <div className="border rounded-xl space-y-4  lg:py-5 m-5 text-center shadow-2xl">
+        <div className="border-3 border-dashed border-[#A9D880] rounded-xl space-y-4  lg:py-5 m-5 text-center shadow-2xl">
           <div className="">
             <img className="w-40 h-full mx-auto " src={image} alt="" />
           </div>
@@ -16,6 +17,10 @@ const Plant = ({ plant }) => {
           </h2>
           <p className="text-lg font-semibold">Category: {category} </p>
           <p className="text-lg text-green-600 pb-2 ">Rating: {rating} </p>
+
+          <button className="px-3 py-2 rounded-lg mx-auto flex gap-3 cursor-pointer text-white font-bold bg-green-800">
+            View Details <img src={arrowImg} alt="" />
+          </button>
         </div>
       </Link>
     </div>

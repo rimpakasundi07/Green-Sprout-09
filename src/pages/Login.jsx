dab2eb696f3f4cfecff6a5b8ec9e540994f43074
rimpa kasundi <rimpakasundi@gmail.com>
2025-11-10 20:58:50 +0530

    added a email placeholder

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -45,6 +45,7 @@ const Login = () => {
                 id="email"
                 name="email"
                 type="email"
+                placeholder="you@example.com"
                 autoComplete="email"
                 required
                 className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
